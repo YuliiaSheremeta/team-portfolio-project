6cfe54ebcf82bf8e580fa2eea0b6e3dc0243b0e7
Yuliia_Sheremeta <sheremeyulya1996@gmail.com>
2025-02-05 13:13:10 +0100

    add img,styles and icon to project

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './js/header.js';
+import './js/hero.js';
+import './js/aboutme.js';
+import './js/benefits.js';
+import './js/projects.js';
+import './js/faq.js';
+import './js/covers.js';
+import './js/reviews.js';
+import './js/worktogether.js';
