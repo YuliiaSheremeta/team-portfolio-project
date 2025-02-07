380bfeb825f4dbc875f7c45fcf2a38dcf61cab1c
TakeItEasy <semen.herasymiuk@gmail.com>
2025-02-07 20:29:45 +0600

    FAQ section to code review

---
+++ src/js/faq.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Accordion("#accordion-faq-left", {
+        duration: 400,
+        showMultiple: true,
+        collapse: true,
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    new Accordion("#accordion-faq-right", {
+        duration: 400,
+        showMultiple: true,
+        collapse: true,
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+      const buttons = document.querySelectorAll('.section-faq-item-button');
+      buttons.forEach(button => {
+        button.addEventListener('click', function() {
+          this.classList.toggle('rotated');
+        });
+      });
+    });
