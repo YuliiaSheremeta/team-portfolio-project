411dca5fbcd9d7b972df49bb835c5348cce18d79
Zhenya Tishakova <tishakovaevgeniya@gmail.com>
2025-02-10 23:29:52 +0200

    Fix: scroll

--- src/js/header.js
+++ src/js/header.js
@@ -10,27 +10,27 @@ if (burgerButton && closeButton && mobileMenuWrapper) {
   document.addEventListener('keydown', handleKeydown);
 }
 
-// function disableScroll() {
-//   const scrollY = window.scrollY;
-//   document.body.style.position = 'fixed';
-//   document.body.style.top = `-${scrollY}px`;
-// }
+function disableScroll() {
+  const scrollY = window.scrollY;
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${scrollY}px`;
+}
 
-// function enableScroll() {
-//   const scrollY = Math.abs(parseInt(document.body.style.top || '0', 10));
-//   document.body.style.position = '';
-//   document.body.style.top = '';
-//   window.scrollTo(0, scrollY);
-// }
+function enableScroll() {
+  const scrollY = Math.abs(parseInt(document.body.style.top || '0', 10));
+  document.body.style.position = '';
+  document.body.style.top = '';
+  window.scrollTo(0, scrollY);
+}
 
 function toggleMobileMenu(e) {
   e.stopPropagation();
   if (mobileMenuWrapper.classList.contains('is-open')) {
     mobileMenuWrapper.classList.remove('is-open');
-    // enableScroll();
+    enableScroll();
   } else {
     mobileMenuWrapper.classList.add('is-open');
-    // disableScroll();
+    disableScroll();
   }
 }
 function handleKeydown(e) {
